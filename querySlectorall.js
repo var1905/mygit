f27b6072a9483d49ebc3f3e7c8b13d3e3dfd700b
var1905 <118063497+var1905@users.noreply.github.com>
2022-12-05 23:43:53 +0530

    odd item green

--- querySlectorall.js
+++ querySlectorall.js
@@ -1,3 +1,16 @@
-let items=document.querySelectorAll('li');
-items[1].style.background="green";
-items[2].style.visibility = "hidden";
+let items=document.querySelectorAll('li')
+
+items[1].style.color="green"
+
+const oddItem=document.querySelectorAll("li:nth-child(odd)");
+
+
+
+
+for(let i=0;i<oddItem.length;i++)
+
+{
+
+    oddItem[i].style.backgroundColor="green";
+
+}
