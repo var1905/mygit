97b6c627c7ae9f237e3af08c7bb8c10c4cecb9be
var1905 <118063497+var1905@users.noreply.github.com>
2022-12-21 19:30:09 +0530

    add user as object in local storage

--- localStorage.js
+++ localStorage.js
@@ -3,11 +3,16 @@ const number=document.getElementById('mobileNumber');
 const email=document.getElementById('email');
 const form=document.getElementById('details');
 form.addEventListener('submit',local)
+let id=1;
 function local(e)
 {
     e.preventDefault();
-    localStorage.setItem('name',name.value)
-    localStorage.setItem('mobileNumber',number.value)
-    localStorage.setItem('email',email.value)
+    let userDtails={
+        name:name.value,
+        number:number.value,
+        email:email.value
 
+    }
+    localStorage.setItem(id,JSON.stringify(userDtails))
+    id++;
 }
