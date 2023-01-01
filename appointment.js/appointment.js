5a29887ab12996362155dae81c24b8418a34b717
var1905 <118063497+var1905@users.noreply.github.com>
2023-01-01 12:31:00 +0530

    edit features

--- appointment.js/appointment.js
+++ appointment.js/appointment.js
@@ -13,56 +13,85 @@ function store(e)
         email
     }
     localStorage.setItem(users.email,JSON.stringify(users))
+
     showData(users);
 
+
+
+
 }
+
+
+
+
 window.addEventListener('DOMContentLoaded',()=>{
+
     const localStoragekeys=Object.keys(localStorage)
+
     const  localStorageObj=localStorage;
-    for(var i=0;i<localStoragekeys.length;i++)
-    {
-        const key=localStoragekeys[i];
-        const userDetailsString =localStorageObj[key];
-        const userDetailsobj=JSON.parse(userDetailsString);
-        showData(userDetailsobj)
-    }
-})
+
+	@@ -31,10 +32,17 @@ window.addEventListener('DOMContentLoaded',()=>{
+
 function showData(user)
 
 {
 
     const parentNode=document.getElementById('listUsers');
 
+    // if(localStorage.getItem(user.email) !== null){
+
+    //     removeUserFromScreen(user.email)
+
+    // }
+
     const childHTML=`<li id=${user.email}> ${user.name} - ${user.email}
 
     <button onclick=deleteuser('${user.email}')>X</button>
 
+    <button onclick=editUser('${user.email}','${user.name}')>✎</button>
+
     </li>`
 
     parentNode.innerHTML=parentNode.innerHTML+childHTML
+
+    //for clear inpusts
+
+    document.getElementById('email').value=""
+
+    document.getElementById('name').value="";
 
 }
 
 function deleteuser(emailId){
 
 localStorage.removeItem(emailId)
-
     removeOnScreem(emailId)
-
 }
 
-
-
-
 function removeOnScreem(emailId)
-
 {
-
     const parentNode=document.getElementById('listUsers')
 
     const childNode=document.getElementById(emailId)
 
     parentNode.removeChild(childNode)
+
+
+
+
+}
+
+function editUser(emailId,name)
+
+{
+
+    document.getElementById('email').value=emailId
+
+    document.getElementById('name').value=name;
+
+    //to ramove wrong details
+
+    deleteuser(emailId)
 
 
 
